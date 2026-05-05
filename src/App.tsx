6d6da8b78eaf382512d039de2ab21d17544c82b2
kngxfrd <amekudzikingsford014@gmail.com>
2026-05-05 10:15:11 +0000

    continuing

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ function App() {
       <header className="header">
         <h1>TO-DO</h1>
       </header>
+      <div className="text-entry">
+        <input
+          className="input-box"
+          type="text"
+          placeholder="...type your task"
+        />
+        <button>Clear</button>
+        <button>Add Task</button>
+      </div>
+      <div className="tasks"></div>
     </div>
   );
 }
