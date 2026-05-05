45a70d094975ed386c6393251eec6058cc1b27fd
kngxfrd <amekudzikingsford014@gmail.com>
2026-05-05 10:11:50 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {} from "./App.css";
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1>TO-DO</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
