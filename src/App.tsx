d1d397ef93020f2dbf9a312e96bb804c54f21cf4
kngxfrd <amekudzikingsford014@gmail.com>
2026-05-07 07:52:36 +0000

    five

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { FaCirclePlus } from "react-icons/fa6";
 import { GoPencil } from "react-icons/go";
 import { TfiTrash } from "react-icons/tfi";
-
+import { IoIosArrowDown } from "react-icons/io";
 function App() {
   type Note = {
     id: number;
@@ -46,73 +46,125 @@ function App() {
   );
   return (
     <div className="app">
-      <div className="head">
+      <div className="text-md text-black font-bold">
         <h3>TODO LIST</h3>
       </div>
-      <div className="searchbox">
+      <div className=" flex item-center justify-center gap-4 pt-5">
         <input
-          className="searchbar"
+          className=" border rounded-sm sm:w-sm h-8 pl-4 border-[#6c63ff] focus:outline-0"
           type="text"
           placeholder="Search note..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button className="filterbutton">All</button>
-      </div>
-      <div className="lists"></div>
-
-      <FaCirclePlus className="plusbutton" onClick={() => setOpen(true)} />
-
-      <div className="lists">
-        {filteredNotes.map((note) => (
-          <div key={note.id} className="note-item">
-            <input
-              className="checkbox"
-              type="checkbox"
-              checked={note.completed}
-              onChange={() =>
-                setNotes(
-                  notes.map((item) =>
-                    item.id === note.id
-                      ? { ...item, completed: !item.completed }
-                      : item,
-                  ),
-                )
-              }
-            />
-            <span className={note.completed ? "done" : ""}>{note.text}</span>
-            <div className="listbutton">
-              <GoPencil
-                onClick={() => {
-                  setEditingNote(note);
-                  setInput(note.text);
-                  setOpen(true);
-                }}
-              />
-              <TfiTrash onClick={() => deleteNote(note.id)} />
-            </div>
+        <button className=" text-md">
+          <div className="border -mt-1 p-2 px-2 py-0.5 flex gap-2 mt-0.5 bg-[#6c63ff] text-white rounded-sm">
+            All
+            <IoIosArrowDown className="mt-1" size={18} />
           </div>
-        ))}
+        </button>
+      </div>
+
+      <div className="flex item-center justify-center pt-2">
+        <div className="w-md  text-center  ">
+          {notes.length === 0 ? (
+            <div>
+              <div className="flex item-center pt-20 justify-center shrink:0 sm:w-auto sm:h-auto ">
+                <img
+                  src="src/assets/emptyimg.png"
+                  alt="No tasks"
+                  className="emptyimg"
+                />
+              </div>
+              <h3>Empty...</h3>
+            </div>
+          ) : filteredNotes.length === 0 ? (
+            <p>No matching tasks found</p>
+          ) : (
+            filteredNotes.map((note) => (
+              <div className="w-md text-center">
+                <div
+                  key={note.id}
+                  className="flex justify-between border-b border-[#6c63ff] py-6"
+                >
+                  <div className="flex items-center">
+                    <input
+                      className="size-6 accent-[#6c63ff] border border-[#6c63ff]"
+                      type="checkbox"
+                      checked={note.completed}
+                      onChange={() =>
+                        setNotes(
+                          notes.map((item) =>
+                            item.id === note.id
+                              ? { ...item, completed: !item.completed }
+                              : item,
+                          ),
+                        )
+                      }
+                    />
+                    <span
+                      className={
+                        note.completed ? "line-through text-gray pl-5" : "pl-5"
+                      }
+                    >
+                      {note.text}
+                    </span>
+                  </div>
+                  <div className="flex gap-3">
+                    <GoPencil
+                      onClick={() => {
+                        setEditingNote(note);
+                        setInput(note.text);
+                        setOpen(true);
+                      }}
+                    />
+                    <TfiTrash onClick={() => deleteNote(note.id)} />
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      <div className="fixed bottom-6 sm:bottom-10 right-5 ml-auto">
+        <FaCirclePlus size={40} color="#6c63ff" onClick={() => setOpen(true)} />
       </div>
 
       {open && (
         <>
-          <div className="overlay"></div>
-          <div className="modalbox">
-            <h3>NEW NOTE</h3>
-            <input
-              type="text"
-              className="notebox"
-              placeholder="Input your note"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <button className="cancelbutton " onClick={() => setOpen(false)}>
-              Cancel
-            </button>
-            <button className="applybutton" onClick={handleApply}>
-              Apply
-            </button>
+          <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+            <div className="bg-white border border-[#6c63ff] rounded-md w-80 p-4 flex flex-col justify-between h-56">
+              <h3 className="text-lg font-semibold">NEW NOTE</h3>
+
+              <input
+                autoFocus
+                type="text"
+                className="border border-[#6c63ff] p-2 w-full rounded focus:outline-0"
+                placeholder="Input your note"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+
+              <div className="flex justify-end gap-35">
+                <button
+                  className="bg-white px-3 py-1 rounded border border-[#6c63ff] text-[#6c63ff]"
+                  onClick={() => {
+                    setOpen(false);
+                    setInput("");
+                    setEditingNote(null);
+                  }}
+                >
+                  Cancel
+                </button>
+
+                <button
+                  className="bg-[#6c63ff] text-white px-3 py-1 rounded"
+                  onClick={handleApply}
+                >
+                  Apply
+                </button>
+              </div>
+            </div>
           </div>
         </>
       )}
